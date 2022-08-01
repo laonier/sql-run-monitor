bb91fdb5aeb3fd28886e6036915659e134ff3e8d
GitHub Action <action@github.com>
2022-08-01 14:12:22 +0000

    Template cleanup

--- src/main/kotlin/com/github/laonier/sqlrunmonitor/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/laonier/sqlrunmonitor/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.laonier.sqlrunmonitor.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.laonier.sqlrunmonitor.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/laonier/sqlrunmonitor/services/MyProjectService.kt
+++ src/main/kotlin/com/github/laonier/sqlrunmonitor/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.laonier.sqlrunmonitor.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.laonier.sqlrunmonitor.MyBundle
 
 class MyProjectService(project: Project) {
 
